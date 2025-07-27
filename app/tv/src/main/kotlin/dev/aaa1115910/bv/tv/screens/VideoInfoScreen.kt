e9bbab3e584d0425fb925e93ce9d3d21d572a80d
fantasytyx <fantacytyx@foxmail.com>
2025-07-28 01:13:21 +0800

    fix bug

--- app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/screens/VideoInfoScreen.kt
+++ app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/screens/VideoInfoScreen.kt
@@ -500,6 +500,9 @@ fun VideoInfoScreen(
                                 delay(300)
                                 fromPlayer = false
                                 intent.removeExtra("fromPlayer")
+                                if (!showUGCVideoInfo) {
+                                    context.finish()
+                                }
                             }
                         }
                         if (!fromPlayer) {
@@ -588,7 +591,7 @@ fun VideoInfoScreen(
             showUGCVideoInfo && 
             !fromPlayer) {
             // 延迟一小段时间确保UI完全渲染
-            delay(100)
+            delay(300)
             defaultFocusRequester.requestFocus(scope)
         }
     }
@@ -647,7 +650,6 @@ fun VideoInfoScreen(
                 )
                 LazyColumn(
                     modifier = Modifier
-                        .focusable()
                         .onKeyEvent { event ->
                              if (event.type == KeyEventType.KeyDown) {
                                  when (event.key) {
@@ -666,7 +668,7 @@ fun VideoInfoScreen(
                              return@onKeyEvent false
                         },
                     state = lazyListState,
-                    contentPadding = PaddingValues(top = 16.dp, bottom = 32.dp),
+                    contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp),
                     verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     item {
@@ -956,7 +958,7 @@ fun ArgueTip(
     Surface(
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 50.dp),
+            .padding(horizontal = 32.dp),
         colors = SurfaceDefaults.colors(
             containerColor = Color.Yellow.copy(alpha = 0.2f),
             contentColor = Color.Yellow
@@ -1015,7 +1017,7 @@ fun VideoInfoData(
 
     Row(
         modifier = modifier
-            .padding(horizontal = 50.dp, vertical = 16.dp),
+            .padding(horizontal = 32.dp, vertical = 16.dp),
     ) {
         Surface(
             modifier = Modifier
@@ -1479,11 +1481,13 @@ fun VideoPartRow(
 
     Column(
         modifier = modifier
-            .ifElse(!nested, Modifier.padding(start = 50.dp))
+            .ifElse(!nested, Modifier.padding(start = 20.dp))
             .onFocusChanged { hasFocus = it.hasFocus },
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
+            modifier = Modifier
+                .padding(start = 12.dp),
             text = stringResource(R.string.video_info_part_row_title)
                     + (" - $subtitle".takeIf { subtitle.isNotBlank() } ?: ""),
             fontSize = titleFontSize.sp,
@@ -1494,7 +1498,7 @@ fun VideoPartRow(
 
         LazyRow(
             modifier = Modifier
-                .padding(top = 15.dp)
+                .padding(top = 10.dp)
                 .focusRestorer(focusRequester),
             contentPadding = PaddingValues(12.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp)
@@ -1555,11 +1559,13 @@ fun VideoUgcSeasonRow(
 
     Column(
         modifier = modifier
-            .padding(start = 50.dp)
+            .padding(start = 20.dp)
             .onFocusChanged { hasFocus = it.hasFocus },
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
+            modifier = Modifier
+                .padding(start = 12.dp),
             text = title,
             fontSize = titleFontSize.sp,
             color = titleColor
@@ -1567,7 +1573,7 @@ fun VideoUgcSeasonRow(
 
         LazyRow(
             modifier = Modifier
-                .padding(top = 15.dp)
+                .padding(top = 10.dp)
                 .focusRestorer(focusRequester),
             contentPadding = PaddingValues(12.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp)
