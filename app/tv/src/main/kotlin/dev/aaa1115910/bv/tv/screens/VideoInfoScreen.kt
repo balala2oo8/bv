7f4d1280444be0c6f6eb49a8aee8c3e90d601b76
fantasytyx <fantacytyx@foxmail.com>
2025-09-12 21:31:48 +0800

    调整各个列表焦点元素停留位置；调整资源卡片样式；调整右侧菜单的样式；减小多个地方的圆角；尝试优化弹幕

--- app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/screens/VideoInfoScreen.kt
+++ app/tv/src/main/kotlin/dev/aaa1115910/bv/tv/screens/VideoInfoScreen.kt
@@ -1053,14 +1053,14 @@ fun VideoInfoData(
                 .padding(4.dp)
                 .unifiedGlow(
                     enabled = coverHasFocus,
-                    shape = MaterialTheme.shapes.large,
+                    shape = MaterialTheme.shapes.medium,
                     glowColor = MaterialTheme.colorScheme.onSurface,
                     glowRadius = 12.dp,
                     glowAlpha = 0.5f
                 ),
             onClick = onClickCover,
             shape = ClickableSurfaceDefaults.shape(
-                shape = MaterialTheme.shapes.large,
+                shape = MaterialTheme.shapes.medium,
             ),
             scale = ClickableSurfaceDefaults.scale(scale = 1f, focusedScale = 1.05f),
             border = ClickableSurfaceDefaults.border(
@@ -1069,7 +1069,7 @@ fun VideoInfoData(
                         width = 2.dp,
                         color = MaterialTheme.colorScheme.border
                     ),
-                    shape = MaterialTheme.shapes.large
+                    shape = MaterialTheme.shapes.medium
                 )
             )
         ) {
@@ -1090,8 +1090,8 @@ fun VideoInfoData(
                         RoundedCornerShape(
                             topStart = 0.dp,
                             topEnd = 0.dp,
-                            bottomStart = 16.dp,
-                            bottomEnd = 16.dp
+                            bottomStart = 12.dp,
+                            bottomEnd = 12.dp
                         )
                     )
                     .background(
